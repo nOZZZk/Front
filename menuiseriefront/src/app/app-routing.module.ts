836fc2d01fbcc899d7a8830ff283c5405ba50b6b
nOZZZk <c.gelant@gmail.com>
2023-10-16 14:22:49 +0200

    create home route

---
+++ menuiseriefront/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+
+const routes: Routes = [{component: HomeComponent,
+path:'**'}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
